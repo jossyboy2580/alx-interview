68077f95afbe803afed9876aaf7f8304e87cf471
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-12-13 09:01:28 +0100

    The prime game to decide who wins if two individuals decide to take turns using the sieve of eratosthenes to cancel out options

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+An implementation of the prime game program
+"""
+
+
+def isWinner(x, nums):
+    """
+    A function that implements the prime game
+
+    This game is played between Ben and Maria
+    """
+
+    players_score = {'Ben': 0, 'Maria': 0}
+
+    if type(nums) != list:
+        return None
+    if len(nums) > x:
+        return None
+    for i in range(x):
+        range_of_numbers = [num for num in range(2, nums[i] + 1, 1)]
+        loop_count = 0
+        while(range_of_numbers):
+            present = range_of_numbers.pop(0)
+            range_of_numbers = [n for n in range_of_numbers if n % present != 0]
+            loop_count += 1
+        if loop_count % 2 == 0:
+            players_score['Ben'] += 1
+        else:
+            players_score['Maria'] += 1
+    if players_score['Ben'] > players_score['Maria']:
+        return 'Ben'
+    elif players_score['Maria'] > players_score['Ben']:
+        return 'Maria'
+    else:
+        return None
+
